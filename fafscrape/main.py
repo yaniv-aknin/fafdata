d4d8ebb1980ce7701f83b33b4f3c0b5edca419e0
Yaniv Aknin <yaniv@aknin.name>
2022-03-12 19:50:07 +0000

    Support multiple input files
    Closes #7

--- fafscrape/main.py
+++ fafscrape/main.py
@@ -4,11 +4,15 @@ import click
 from .transform import transform_game, index_inclusions
 
 @click.command()
-@click.argument('input', type=click.File('r'))
-@click.argument('output', type=click.File('w'), default='-')
-def faf_dump_to_bigquery_jsonl(input, output):
-    raw = json.load(input)
-    inclusions = index_inclusions(raw['included'])
-    for game in raw['data']:
-        assert game['type'] == 'game'
-        output.write(json.dumps(transform_game(game, inclusions)) + '\n')
+@click.argument('inputs', type=click.File('r', lazy=True), nargs=-1)
+@click.argument('output', type=click.File('w'), nargs=1)
+def faf_dump_to_bigquery_jsonl(inputs, output):
+    with click.progressbar(inputs, label='Transforming') as bar:
+        for input in bar:
+            raw = json.load(input)
+            inclusions = index_inclusions(raw['included'])
+            for game in raw['data']:
+                assert game['type'] == 'game'
+                output.write(json.dumps(transform_game(game, inclusions)) + '\n')
+            input.close()
+        output.close()
