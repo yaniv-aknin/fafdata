bc55b9dc2f4023ba0189d423d37369f182cc99a6
Yaniv Aknin <yaniv@aknin.name>
2022-03-12 16:13:06 +0000

    transform: include playerId in gamePlayerStats
    Also adds a few more keys I forgot to transform and adds a test.
    
    closes #6

--- fafscrape/transform.py
+++ fafscrape/transform.py
@@ -7,23 +7,26 @@ def index_inclusions(inclusions):
         inclusion_id = inclusion['id']
         if inclusion_type not in result:
             result[inclusion_type] = {}
-        result[inclusion_type][inclusion_id] = inclusion['attributes'].copy()
+        result[inclusion_type][inclusion_id] = inclusion
     return result
 
 def get_included(inclusions, i_type, i_id):
-    return inclusions[i_type][i_id].copy()
+    return inclusions[i_type][i_id]
 
 def transform_game(game, inclusions):
     result = game['attributes'].copy()
     result['endTime'] = faf_to_bigquery_datetime(result['endTime'])
     result['startTime'] = faf_to_bigquery_datetime(result['startTime'])
     result['id'] = game['id']
-    result['featuredMod'] = game['relationships']['featuredMod']['data']['id']
+    result['featuredModId'] = game['relationships']['featuredMod']['data']['id']
+    # the 'or' is a bit of a hack, mapVersion is sometimes (rarely) null; see e.g. game ID 459322
+    result['mapVersionId'] = (game['relationships']['mapVersion']['data'] or {'id': None})['id']
     result['playerStats'] = []
     ps_refs = game['relationships'].get('playerStats', {'data': []})
     for ref in ps_refs['data']:
-        player_stats = get_included(inclusions, 'gamePlayerStats', ref['id'])
-        player_stats['scoreTime'] = faf_to_bigquery_datetime(player_stats['scoreTime'])
-        result['playerStats'].append(player_stats)
+        player_stats = inclusions['gamePlayerStats'][ref['id']]
+        record = player_stats['attributes'].copy()
+        record['scoreTime'] = faf_to_bigquery_datetime(record['scoreTime'])
+        record['playerId'] = player_stats['relationships']['player']['data']['id']
+        result['playerStats'].append(record)
     return result
-
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -2,13 +2,28 @@ import pytest
 import json
 
 import testutils
-from fafscrape.transform import index_inclusions
+from fafscrape.transform import index_inclusions, transform_game
 
 def test_index_inclusions(api_dump):
     inclusions = index_inclusions(api_dump['included'])
     assert 'gamePlayerStats' in inclusions
     assert 'player' in inclusions
     assert '28030408' in inclusions['gamePlayerStats']
+
+def test_transform_game(api_dump):
+    inclusions = index_inclusions(api_dump['included'])
+    raw_game = api_dump['data'][0]
+    xform_game = transform_game(raw_game, inclusions)
+    assert xform_game['name'] == 'GreatGrapes Vs Pistachios'
+    assert xform_game['featuredModId'] == '6'
+    assert len(xform_game['playerStats']) == 2
+    assert xform_game['playerStats'][0]['playerId'] == '405147'
+
+def test_transform_games(api_dump):
+    inclusions = index_inclusions(api_dump['included'])
+    for raw_game in api_dump['data']:
+        xform_game = transform_game(raw_game, inclusions)
+        assert 'id' in xform_game
 
 @pytest.fixture
 def api_dump():
