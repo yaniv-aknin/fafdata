4f88aa5edd0fff10015005ae8b00dbcdabd557c6
Yaniv Aknin <yaniv@aknin.name>
2022-03-13 10:35:57 +0000

    add basic logging
    Should be useful when deploying in issue #5

--- fafscrape/webapp.py
+++ fafscrape/webapp.py
@@ -20,14 +20,18 @@ def index():
 
 @app.route('/scrape/games', methods=['POST', 'GET'] if app.debug else ['POST'])
 def scrape_games():
-    def generate(constructor, max_pages):
-        for page in yield_pages(constructor, max_pages=max_pages):
-            for game in process_games_page(page):
-                yield (json.dumps(game) + '\n')
     page_size = int(flask.request.args.get('page_size', DEFAULT_PAGE_SIZE))
     per_page_sleep = int(flask.request.args.get('per_page_sleep', DEFAULT_PER_PAGE_SLEEP))
     max_pages = int(flask.request.args.get('max_pages', 0)) or float('inf')
     start_date = parse_date(flask.request.args.get('start_date', DEFAULT_TWO_DAYS_AGO))
+    app.logger.info('invoked page_size=%d per_page_sleep=%d max_pages=%s start_date=%s', page_size, per_page_sleep, max_pages, start_date)
+
+    def generate(constructor, max_pages):
+        for page in yield_pages(constructor, max_pages=max_pages):
+            meta = page['meta']['page']
+            app.logger.info('processing page %d of %d (%d rows)', meta['number'], meta['totalPages'], meta['totalRecords'])
+            for game in process_games_page(page):
+                yield (json.dumps(game) + '\n')
     constructor = functools.partial(construct_url, 'game', ['playerStats'], 'endTime', start_date, page_size)
     body = generate(constructor, max_pages)
     if app.debug:
