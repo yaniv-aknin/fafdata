d9db0a2cf9870edd0e4fb1590335ac6d162a494a
Yaniv Aknin <yaniv@aknin.name>
2022-03-14 07:17:18 +0000

    implement bigquery load from webapp
    This completes a full ETL cycle from the webapp, and means we can try
    scheduling this thing daily to start accumulating data.
    
    Fixes #2

--- fafscrape/webapp.py
+++ fafscrape/webapp.py
@@ -1,4 +1,8 @@
+import os
+import time
 import json
+import tempfile
+import pathlib
 import functools
 import flask
 import requests
@@ -6,6 +10,7 @@ import requests
 from .fetch import construct_url, yield_pages
 from .transform import process_page
 from .utils import parse_date, DEFAULT_TWO_DAYS_AGO, DEFAULT_ONE_DAY_AGO
+from .load import get_client, load_table_from_iterable
 
 DEFAULT_PAGE_SIZE=10
 DEFAULT_FIRST_PAGE=1
@@ -17,6 +22,7 @@ app = flask.Flask(__name__)
 def index():
     return 'Use /scrape/<entity> to operate this tool'
 
+# refuse 'GET' unless in debug mode because GET should be idempotent (but for debug it's easier to invoke)
 @app.route('/scrape/games', methods=['POST', 'GET'] if app.debug else ['POST'])
 def scrape_games():
     page_size = int(flask.request.args.get('page_size', DEFAULT_PAGE_SIZE))
@@ -31,10 +37,13 @@ def scrape_games():
         for page in yield_pages(constructor, max_pages=max_pages):
             meta = page['meta']['page']
             app.logger.info('processing page %d of %d (%d rows)', meta['number'], meta['totalPages'], meta['totalRecords'])
-            for game in process_page(page):
-                yield (json.dumps(game) + '\n')
+            yield from process_page(page)
+            time.sleep(per_page_sleep)
+
+    # creating the client can fail if credentials aren't set up, so we intentionally do it early before doing any FAF API traffic
+    client = get_client()
     constructor = functools.partial(construct_url, 'game', ['playerStats'], 'endTime', page_size, start_date, end_date)
-    body = generate(constructor, max_pages)
-    if app.debug:
-        body = "".join(body)
-    return app.response_class(body, mimetype='application/jsonl+json')
+    table_id = os.environ['BIGQUERY_GAME_TABLE_ID']
+    job = load_table_from_iterable(client, table_id, generate(constructor, max_pages))
+    job.result()
+    return str((job.job_id, job.state))
---
+++ runcommands.sh
@@ -0,0 +1,2 @@
+export GOOGLE_APPLICATION_CREDENTIALS="$VIRTUAL_ENV/google-credentials.json"
+export BIGQUERY_GAME_TABLE_ID='fafalytics.faf.games'
