fb5bd901678a5fd8af19eae2f312ba3af69632be
Yaniv Aknin <yaniv@aknin.name>
2022-06-25 07:12:10 +0100

    test: add test utility to delete some lines

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,7 @@ import responses
 
 import conftest
 import json
+import contextlib
 
 from fafdata.main import extract_from_faf_api, transform_api_dump_to_jsonl, parse_replays_to_pickle, dump_replay_commands_to_jsonl
 from fafdata.utils import decompressed
@@ -14,11 +15,17 @@ def read_jsonl(path, num_lines):
     with decompressed(path) as handle:
         return [json.loads(handle.readline()) for x in range(num_lines)]
 
-def test_parse_replays_to_pickle():
-    replay_path = str(conftest.testdata / 'replay.v2.fafreplay')
+@contextlib.contextmanager
+def isolated_file(filename=None, pre_cmd=None):
+    path = str(conftest.testdata / filename) if filename else '/dev/null'
     runner = CliRunner()
     with runner.isolated_filesystem():
-        os.system(f'cp {replay_path} .')
+        if pre_cmd:
+            os.system(pre_cmd.format(path=path))
+        yield runner, path
+
+def test_parse_replays_to_pickle():
+    with isolated_file('replay.v2.fafreplay', 'cp {path} .') as (runner, path):
         result = runner.invoke(parse_replays_to_pickle, ['./replay.v2.fafreplay'])
         assert result.exit_code == 0
         with decompressed('./replay.v2.pickle.zstd') as handle:
@@ -26,29 +33,21 @@ def test_parse_replays_to_pickle():
             assert 'json' in data
 
 def test_parse_replays_to_pickle_errors():
-    replay_path = str(conftest.testdata / 'replay.v2.fafreplay')
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.system(f'dd if=/dev/urandom of=invalid.fafreplay bs=1k count=16')
+    dd_cmd = 'dd if=/dev/urandom of=invalid.fafreplay bs=1k count=16'
+    with isolated_file('replay.v2.fafreplay', dd_cmd) as (runner, path):
         result = runner.invoke(parse_replays_to_pickle, ['./invalid.fafreplay', '--no-ignore-errors'])
         assert result.exit_code == 1
         result = runner.invoke(parse_replays_to_pickle, ['./invalid.fafreplay', '--ignore-errors'])
         assert result.exit_code == 0
 
 def test_dump_replay_commands_to_jsonl():
-    replay_path = str(conftest.testdata / 'replay.v2.fafreplay')
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.system(f'cp {replay_path} .')
+    with isolated_file('replay.v2.fafreplay', 'cp {path} .') as (runner, path):
         result = runner.invoke(dump_replay_commands_to_jsonl, ['./replay.v2.fafreplay', 'out.jsonl'])
         assert result.exit_code == 0
         assert read_jsonl('out.jsonl', 1)[0]['id'] == '12519949'
 
 def test_dump_replay_commands_to_jsonl_compressed():
-    replay_path = str(conftest.testdata / 'replay.v2.fafreplay')
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.system(f'cp {replay_path} .')
+    with isolated_file('replay.v2.fafreplay', 'cp {path} .') as (runner, path):
         result = runner.invoke(dump_replay_commands_to_jsonl, ['./replay.v2.fafreplay', 'out.jsonl.gz'])
         assert result.exit_code == 0
         assert read_jsonl('out.jsonl.gz', 1)[0]['id'] == '12519949'
@@ -56,32 +55,23 @@ def test_dump_replay_commands_to_jsonl_compressed():
         assert open('out.jsonl.gz', 'rb').read(2) == GZ_MAGIC
 
 def test_dump_replay_commands_to_jsonl_directory():
-    replay_path = str(conftest.testdata / 'replay.v2.fafreplay')
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.system(f'mkdir foo ; cp {replay_path} foo')
+    with isolated_file('replay.v2.fafreplay', 'mkdir foo ; cp {path} foo') as (runner, path):
         result = runner.invoke(dump_replay_commands_to_jsonl, ['foo', 'out.jsonl'])
         assert result.exit_code == 0
         assert read_jsonl('out.jsonl', 1)[0]['id'] == '12519949'
 
 def test_transform_api_dump_to_jsonl_game():
-    dump_path = conftest.testdata / 'games.json'
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
-        result = runner.invoke(transform_api_dump_to_jsonl, [str(dump_path), 'output'])
+    with isolated_file('games.json', 'mkdir output') as (runner, path):
+        result = runner.invoke(transform_api_dump_to_jsonl, [path, 'output'])
         assert result.exit_code == 0
         xformed, = read_jsonl('output/xformed.jsonl', 1)
         assert xformed['id'] == '14395974'
         assert xformed['mapVersion_mapVersion_id'] == '18852'
 
 def test_transform_api_dump_to_jsonl_game_deduped():
-    dump_path = conftest.testdata / 'games.json'
-    games = json.load(open(dump_path))
-    games['data'].insert(1, copy.deepcopy(games['data'][0]))
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
+    with isolated_file('games.json', 'mkdir output') as (runner, path):
+        games = json.load(open(path))
+        games['data'].insert(1, copy.deepcopy(games['data'][0]))
         json.dump(games, open('games_duplicate.json', 'w'))
         result = runner.invoke(transform_api_dump_to_jsonl, ['games_duplicate.json', 'output'])
         assert result.exit_code == 0
@@ -96,30 +86,21 @@ def test_transform_api_dump_to_jsonl_game_deduped():
         assert g1['id'] == g2['id']
 
 def test_transform_api_dump_to_jsonl_game_gzipped():
-    dump_path = conftest.testdata / 'games.json'
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
-        os.system(f'cp {dump_path} . && gzip games.json')
+    with isolated_file('games.json', 'mkdir output') as (runner, path):
+        os.system(f'cp {path} . && gzip games.json')
         result = runner.invoke(transform_api_dump_to_jsonl, ['games.json.gz', 'output'])
         assert result.exit_code == 0
 
 def test_transform_api_dump_to_jsonl_game_partitioned():
-    dump_path = conftest.testdata / 'games.json'
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
-        result = runner.invoke(transform_api_dump_to_jsonl, ['--partition-strategy', 'year_month', str(dump_path), 'output'])
+    with isolated_file('games.json', 'mkdir output') as (runner, path):
+        result = runner.invoke(transform_api_dump_to_jsonl, ['--partition-strategy', 'year_month', path, 'output'])
         assert result.exit_code == 0
         xformed, = read_jsonl('output/dt=2012-01-01/2012-12-01.jsonl', 1)
         assert xformed['id'] == '459322'
 
 def test_transform_api_dump_to_jsonl_player():
-    dump_path = conftest.testdata / 'players.json'
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
-        result = runner.invoke(transform_api_dump_to_jsonl, [str(dump_path), 'output'])
+    with isolated_file('players.json', 'mkdir output') as (runner, path):
+        result = runner.invoke(transform_api_dump_to_jsonl, [path, 'output'])
         assert result.exit_code == 0
         xformed, = read_jsonl('output/xformed.jsonl', 1)
         assert xformed['id'] == '368434'
@@ -129,18 +110,14 @@ def test_extract_from_faf_api(games_json):
     url = ('https://api.faforever.com/data/game?page%5Bsize%5D=10&page%5Bnumber%5D=1&page%5Btotals%5D=&'
            'filter=startTime%3Dge%3D1970-01-01T00%3A00%3A00Z%3BstartTime%3Dle%3D1970-01-01T00%3A00%3A00Z&sort=startTime')
     responses.add(method='GET', url=url, json=games_json)
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
+    with isolated_file(pre_cmd='mkdir output') as (runner, path):
         result = runner.invoke(extract_from_faf_api, ['output', 'game', '--start-date=1970-01-01', '--end-date=1970-01-01', '--max-pages=1'])
         assert result.exit_code == 0
         assert set(os.listdir('output')) == {'game.metadata.json', 'game0001.json'}
 
 @responses.activate
 def test_extract_from_faf_api_date_field(games_json):
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.mkdir('output')
+    with isolated_file(pre_cmd='mkdir output') as (runner, path):
         result = runner.invoke(extract_from_faf_api, ['output', 'foo', '--start-date=1970-01-01', '--end-date=1970-01-01', '--max-pages=1'])
         # should fail because the unknown entity 'foo' requires setting --date-field
         assert result.exit_code == 2
